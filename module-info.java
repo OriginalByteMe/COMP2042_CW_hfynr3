aeaf5679b5f2f74ca87ea305a2ff4c1adb461f71
Noah R <noahrijkaard@gmail.com>
2020-12-05 13:45:32 +0800

    Create module-info.java

---
+++ module-info.java
@@ -0,0 +1,10 @@
+module Noah.Frogger {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.media;
+    requires javafx.graphics;
+    requires javafx.swing;
+
+
+    opens p4_group_8_repo;
+}
